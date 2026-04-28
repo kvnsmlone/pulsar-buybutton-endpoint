bc773b257b6f934e25429530e51bf2b6175f42bf
kvnsmlone <Kvnsml@yahoo.com>
2026-04-28 12:05:14 -0700

    Update track-affiliate.js
    TEST X

--- netlify/functions/track-affiliate.js
+++ netlify/functions/track-affiliate.js
@@ -1,32 +1,9 @@
-exports.handler = async function(event, context) {
-  try {
-    const params = event.queryStringParameters || {};
+exports.handler = async (event) => {
+  console.log("🔥 WEBHOOK HIT");
+  console.log("BODY:", event.body);
 
-    const email = params.email || "";
-    const amount = params.amount || "";
-    const orderId = params.orderId || "";
-    const code = params.code || "";
-
-    const url = `https://static.leaddyno.com/track-purchase`
-      + `?key=ef0c0534045d3561102d7abcfc1ea64413e8f8a7`
-      + `&email=${encodeURIComponent(email)}`
-      + `&amount=${encodeURIComponent(amount)}`
-      + `&code=${encodeURIComponent(code)}`
-      + `&purchase_code=${encodeURIComponent(orderId)}`;
-
-    console.log("Calling LeadDyno pixel:", url);
-
-    await fetch(url);
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ success: true })
-    };
-
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message })
-    };
-  }
+  return {
+    statusCode: 200,
+    body: "OK"
+  };
 };
